86f48ffc4c53d07fe1bdbe64b5763f62243935c2
“lo56ve <1046866318@qq.com>
2017-12-06 23:44:17 +0800

    首页建设中...

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,23 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <banner></banner>
+    <router-view></router-view>
+    <tabFoot></tabFoot>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+  import banner from './components/banner.vue'
+  import tabFoot from './components/tabFoot.vue'
+
+  export default {
+    name: 'app',
+    components: {
+      banner,
+      tabFoot
+    }
+  }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/banner.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <img src="/static/images/head.png" alt="">
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+  img {
+    width: 100%;
+  }
+</style>
