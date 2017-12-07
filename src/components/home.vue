0c8ddec584d8920e18d3f093b994da9340a8e56d
“lo56ve <1046866318@qq.com>
2017-12-08 00:01:37 +0800

    添加vue-m-carousel组件实现轮播

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,13 +1,41 @@
 <template>
     <div>
-      首页
+      <Carousel>
+        <div v-for="i in 3" :indicators="true" :auto="3000">
+          <img :src="'/static/images/carousel/' + i + '.jpg'" alt="">
+        </div>
+      </Carousel>
     </div>
 </template>
 
 <script>
-  export default {}
+  import Carousel from 'vue-m-carousel'
+
+  export default {
+    data () {
+      return {
+        carouselArr: []
+      }
+    },
+    mounted () {
+      this.carouselArr = [
+        '/static/images/carousel/1.jpg',
+        '/static/images/carousel/2.jpg',
+        '/static/images/carousel/3.jpg'
+      ]
+    },
+    components: {
+      Carousel
+    }
+  }
 </script>
 
-<style>
+<style lang="scss" scoped>
+  $theme-color: rgb(255, 195, 194);
 
+  .carousel-wrap {
+    margin-top: 7px;
+    border-top: 1px solid $theme-color;
+    border-bottom: 1px solid $theme-color;
+  }
 </style>
