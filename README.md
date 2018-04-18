aa56adfa6b8f811919fa4ea76cbe025ea33c8bfd
lo56ve <1046866318@qq.com>
2018-04-18 17:47:03 +0800

    mock添加完成

--- README.md
+++ README.md
@@ -8,6 +8,6 @@
 ### step
 
 - npm install     [安装依赖]
-- npm run dev     [运行前端项目]
 - npm run mock    [运行mock模拟数据]
+- npm run dev     [运行前端项目]
 - npm run build   [生产环境，打包文件在`/index`]
--- mock/product.js
+++ mock/product.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const Mock = require('mockjs')
 const Random = Mock.Random
 
+// 产品列表
 router.get('/productlist', (req, res) => {
   let category_list = [
     {category_id: '1', name: '唇部'},
@@ -40,6 +41,41 @@ router.get('/productlist', (req, res) => {
       category_list,
       product_list
     },
+    description: 'OK',
+    description_cn: '',
+    errcode: 0
+  })
+})
+
+// 产品详细信息
+router.get('/info', (req, res) => {
+  res.json({
+    data: {
+      category: Random.cword(2),
+      category_id: `${Math.round(Math.random() * 3 + 1)}`,
+      content: `<img src=${Random.image('600x400')}>`,
+      name: Random.word(3, 7),
+      type: Random.cword(2),
+      type_id: '1'
+    },
+    description: 'OK',
+    description_cn: '',
+    errcode: 0
+  })
+})
+
+// 搜索
+router.get('/search', (req, res) => {
+  let productArr = []
+  for(let i = 0; i < 4; i++) {
+    productArr.push({
+      id: `${Math.round(Math.random() * 3 + 1)}`,
+      img_url: Random.image('200x200'),
+      name: `${req.query.key}_${Random.word(3, 4)}`
+    })
+  }
+  res.json({
+    data: productArr,
     description: 'OK',
     description_cn: '',
     errcode: 0
